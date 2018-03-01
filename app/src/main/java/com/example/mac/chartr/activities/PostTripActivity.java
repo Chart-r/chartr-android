fbb1cc1e51fdd305238bf7a88ccea8cdcf5e94a5
pgalusza <pgalusza@gmail.com>
2018-02-28 18:52:47 -0600

    Created activity for posting trip.
    Created PostTripActivity, updated AndroidManifest.xml.

---
+++ app/src/main/java/com/example/mac/chartr/activities/PostTripActivity.java
@@ -0,0 +1,28 @@
+package com.example.mac.chartr.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.mac.chartr.R;
+
+public class PostTripActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_trip);
+    }
+
+    public void incrementSeats(View view){
+
+    }
+
+    public void decrementSeats(View view){
+
+    }
+
+    public void postTrip(View view) {
+
+    }
+}
