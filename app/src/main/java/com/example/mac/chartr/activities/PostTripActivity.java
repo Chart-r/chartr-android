37d734b7950d607990ef6e982800ef1f43679b28
pgalusza <pgalusza@gmail.com>
2018-02-28 19:23:28 -0600

    Increment and Decrement button functionality implemented.

--- app/src/main/java/com/example/mac/chartr/activities/PostTripActivity.java
+++ app/src/main/java/com/example/mac/chartr/activities/PostTripActivity.java
@@ -3,10 +3,41 @@ package com.example.mac.chartr.activities;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.TextView;
 
 import com.example.mac.chartr.R;
 
+import org.w3c.dom.Text;
+
+import java.sql.Time;
+import java.util.Date;
+
 public class PostTripActivity extends AppCompatActivity {
+
+    private EditText inStartLocation;
+    private EditText inEndLocation;
+    private EditText inDepartureDate;
+    private EditText inReturnDate;
+    private EditText inDepartureTime;
+    private EditText inReturnTime;
+    private Switch inCanPickUp;
+    private TextView inNumSeats;
+    private Switch inAllowSmoking;
+    private Switch inIsQuiet;
+
+    private String startLocation;
+    private String endLocation;
+    private Date departureDate;
+    private Date returnDate;
+    private Time departureTime;
+    private Time returnTime;
+    private boolean canPickUp;
+    private int numSeats;
+    private boolean allowSmoking;
+    private boolean isQuiet;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,11 +46,19 @@ public class PostTripActivity extends AppCompatActivity {
     }
 
     public void incrementSeats(View view){
-
+        inNumSeats = (TextView) findViewById(R.id.seatValue);
+        numSeats = Integer.parseInt(inNumSeats.getText().toString());
+        if(numSeats < 4) {
+            inNumSeats.setText(numSeats + 1);
+        }
     }
 
     public void decrementSeats(View view){
-
+        inNumSeats = (TextView) findViewById(R.id.seatValue);
+        numSeats = Integer.parseInt(inNumSeats.getText().toString());
+        if(numSeats > 2) {
+            inNumSeats.setText(numSeats - 1);
+        }
     }
 
     public void postTrip(View view) {
