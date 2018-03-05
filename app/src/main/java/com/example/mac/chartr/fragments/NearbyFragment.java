f321aebf47849a8fcbdc3a0b8e5c7ef517f71175
pgalusza <pgalusza@gmail.com>
2018-03-04 22:34:32 -0600

    Minor changes and bug fixes.

--- app/src/main/java/com/example/mac/chartr/fragments/NearbyFragment.java
+++ app/src/main/java/com/example/mac/chartr/fragments/NearbyFragment.java
@@ -36,43 +36,7 @@ public class NearbyFragment extends Fragment {
         ((AppCompatActivity) getActivity()).findViewById(R.id.buttonAddTrip).setVisibility(View.GONE);
 
         View root = inflater.inflate(R.layout.fragment_nearby, container, false);
-
-        // Populate scrollview
-        LinearLayout tripsLinearLayout = root.findViewById(R.id.tripsLinearLayout);
-        // TODO: implement api call for get trip, store formatted result in trips variable
-        // Test data to show that functionality is working
-        Trip[] trips = {
-                new Trip("Start Time", "End Time", false, true, 0, 0, 0, 0, 4, (float) 0, "id", new User[]{AppHelper.getLoggedInUser()})
-        };
-
-        for (Trip trip : trips) {
-            addTripView(tripsLinearLayout, trip);
-        }
-
         return root;
     }
 
-    /**
-     * Adds an individual trip view to the linear layout passed in.
-     * Duplicate of this method is in ListTripsFragment, possible refactoring in future.
-     */
-    private void addTripView(LinearLayout parentLayout, Trip trip) {
-        //create a view to inflate the layout_item (the xml with the textView created before)
-        View tripContainer = getLayoutInflater().inflate(R.layout.layout_trip_container, parentLayout,false);
-
-        // Set TextViews with appropriate data
-        String name = AppHelper.getLoggedInUser().getName();
-        String rating = String.valueOf(AppHelper.getLoggedInUser().getRating());
-        String seats = String.valueOf(trip.getSeats());
-        String start = String.valueOf(trip.getStartLat()) + "," + String.valueOf(trip.getStartLong());
-        String destination = String.valueOf(trip.getEndLat()) + "," + String.valueOf(trip.getEndLong());
-
-        ((TextView) tripContainer.findViewById(R.id.textViewName)).setText(name);
-        ((TextView) tripContainer.findViewById(R.id.textViewRating)).setText(rating);
-        ((TextView) tripContainer.findViewById(R.id.textViewSeats)).setText(seats);
-        ((TextView) tripContainer.findViewById(R.id.textViewStart)).setText(start);
-        ((TextView) tripContainer.findViewById(R.id.textViewDestination)).setText(destination);
-
-        parentLayout.addView(tripContainer);
-    }
 }
