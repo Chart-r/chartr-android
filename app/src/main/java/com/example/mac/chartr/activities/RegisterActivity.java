fbc8d679d96cf0fc1f58703db69b183f563ac1dc
Christian Cygnus <gamer00@att.net>
2018-04-13 20:43:26 -0500

    Added full error checks on input

--- app/src/main/java/com/example/mac/chartr/activities/RegisterActivity.java
+++ app/src/main/java/com/example/mac/chartr/activities/RegisterActivity.java
@@ -300,8 +300,8 @@ public class RegisterActivity extends AppCompatActivity {
                         Pattern.compile("^\\+1([1-9])(\\d{9})");
                 Matcher matcher = pattern.matcher(phone.getText().toString());
 
-                if (matcher.find() || (phone.getText().toString().length() > 2 &&
-                        phone.getText().toString().charAt(1) != '1')) {
+                if (matcher.find() || (phone.getText().toString().length() > 2
+                        && phone.getText().toString().charAt(1) != '1')) {
                     message.setText("");
                 } else {
                     message.setText("Invalid phone number");
@@ -343,13 +343,13 @@ public class RegisterActivity extends AppCompatActivity {
                 String enteredBirthday = birthday.getText().toString();
                 //https://stackoverflow.com/questions/5978510/regex-to-match-date
                 Pattern pattern =
-                        Pattern.compile("(?:(09|04|06|11)([/])(0[1-9]|[12]\\d|30)" +
-                                "([/])((?:19|20)\\d\\d))|(?:(01|03|05|07|08|10|12)" +
-                                "([/])(0[1-9]|[12]\\d|3[01])([/])((?:19|20)\\d\\d))" +
-                                "|(?:02([/])(?:(?:(0[1-9]|1\\d|2[0-8])([/])" +
-                                "((?:19|20)\\d\\d))|(?:(29)([/])" +
-                                "((?:(?:19|20)(?:04|08|12|16|20|24|28|32|36|40|44|48|52|56|" +
-                                "60|64|68|72|76|80|84|88|92|96))|2000))))");
+                        Pattern.compile("(?:(09|04|06|11)([/])(0[1-9]|[12]\\d|30)"
+                                + "([/])((?:19|20)\\d\\d))|(?:(01|03|05|07|08|10|12)"
+                                + "([/])(0[1-9]|[12]\\d|3[01])([/])((?:19|20)\\d\\d))"
+                                + "|(?:02([/])(?:(?:(0[1-9]|1\\d|2[0-8])([/])"
+                                + "((?:19|20)\\d\\d))|(?:(29)([/])"
+                                + "((?:(?:19|20)(?:04|08|12|16|20|24|28|32|36|40|44|48|52|56|"
+                                + "60|64|68|72|76|80|84|88|92|96))|2000))))");
                 Matcher matcher = pattern.matcher(enteredBirthday);
 
 
@@ -445,9 +445,12 @@ public class RegisterActivity extends AppCompatActivity {
             public void onTextChanged(CharSequence s, int start, int before, int count) {
                 TextView message = (TextView) findViewById(R.id.textViewUserRegPasswordMessage);
 
+
+                //https://stackoverflow.com/questions/19605150/
+                // regex-for-password-must-contain-at-least-eight-characters-at-least-one-number-a
                 Pattern pattern =
-                        Pattern.compile("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)" +
-                                "(?=.*[$@$!%*?&])[A-Za-z\\d$@$!%*?&]{8,}");
+                        Pattern.compile("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)"
+                                + "(?=.*[$@$!%*?&])[A-Za-z\\d$@$!%*?&]{8,}");
                 Matcher matcher = pattern.matcher(password.getText().toString());
 
                 if (matcher.find()) {
@@ -489,16 +492,17 @@ public class RegisterActivity extends AppCompatActivity {
             public void onTextChanged(CharSequence s, int start, int before, int count) {
                 TextView message = (TextView) findViewById(R.id.textViewRegEmailMessage);
 
+                //http://emailregex.com/
                 Pattern pattern =
-                        Pattern.compile("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\." +
-                                "[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"" +
-                                "(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\" +
-                                "x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9]" +
-                                "(?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\" +
-                                "[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]" +
-                                "|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\" +
-                                "x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\" +
-                                "x09\\x0b\\x0c\\x0e-\\x7f])+)\\])");
+                        Pattern.compile("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\."
+                                + "[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\""
+                                + "(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\"
+                                + "x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9]"
+                                + "(?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\"
+                                + "[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]"
+                                + "|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\"
+                                + "x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\"
+                                + "x09\\x0b\\x0c\\x0e-\\x7f])+)\\])");
                 Matcher matcher = pattern.matcher(email.getText().toString());
 
                 if (matcher.find()) {
