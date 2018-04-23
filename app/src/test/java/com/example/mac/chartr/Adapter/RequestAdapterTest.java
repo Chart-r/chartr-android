8d72ef3b6565311f0b9b816057c8e2a36696b97e
Michael Rush <michaelrush1@gmail.com>
2018-04-22 21:40:40 -0500

    Fixed Checkstyle, and started to test requestAdapter

---
+++ app/src/test/java/com/example/mac/chartr/Adapter/RequestAdapterTest.java
@@ -0,0 +1,37 @@
+package com.example.mac.chartr.Adapter;
+
+
+
+import com.example.mac.chartr.adapters.RequestAdapter;
+
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+
+
+/**
+ * Created by Michael Rush on 4/22/2018.
+ */
+
+public class RequestAdapterTest {
+    @Test
+    public void testSetRiderStatus() {
+        RequestAdapter test = new RequestAdapter(null);
+
+    }
+
+    @Test
+    public void testGetItemCount() {
+//        List<Pair<Trip, User>> requestedUsers = null;
+//        Trip t = new Trip(1.0,1.0,true,true,6.0,6.0,6.0,6.0,2);
+//        User u = new User();
+//        Pair<Trip,User> p = new Pair(t,u);
+//        requestedUsers.add(p);
+//
+//        RequestAdapter test = new RequestAdapter(requestedUsers);
+//        Assert.assertEquals(1,test.getItemCount());
+        Assert.assertTrue(true);
+    }
+}
--- app/src/test/java/com/example/mac/chartr/objects/ReviewTest.java
+++ app/src/test/java/com/example/mac/chartr/objects/ReviewTest.java
@@ -1,6 +1,5 @@
 package com.example.mac.chartr.objects;
 
-import android.util.Log;
 
 import junit.framework.Assert;
 
@@ -12,10 +11,10 @@ import org.junit.Test;
 
 public class ReviewTest {
     @Test
-    public void testEquals(){
-        Review r1 = new Review("1","2","3","4","5",6.0);
-        Review r2 = new Review("1","2","3","4","5",6.0);
-        Review r3 = new Review("1","2","3","4","5",7.0);
+    public void testEquals() {
+        Review r1 = new Review("1", "2", "3", "4", "5", 6.0);
+        Review r2 = new Review("1", "2", "3", "4", "5", 6.0);
+
         Assert.assertTrue(r1.equals(r2));
         r2.setComment("fail");
         Assert.assertFalse(r1.equals(r2));
@@ -32,10 +31,10 @@ public class ReviewTest {
     }
 
     @Test
-    public void testHashCode(){
+    public void testHashCode() {
         Review r = new Review();
         r.setRid("hi");
         int hash = r.hashCode();
-        Assert.assertEquals(hash,817163167);
+        Assert.assertEquals(hash, 817163167);
     }
 }
