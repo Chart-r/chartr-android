a48510cbe005b1e9d8593cd532bcc0e75f4fd5e8
Christian Cygnus <gamer00@att.net>
2018-04-27 18:45:38 -0500

    Fix checkstyle

--- app/src/test/java/com/example/mac/chartr/objects/ConfirmationEmailTest.java
+++ app/src/test/java/com/example/mac/chartr/objects/ConfirmationEmailTest.java
@@ -4,8 +4,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-public class ConfirmationEmailTest
-{
+public class ConfirmationEmailTest {
 
     @Test
     public void testGetAndSet() {
